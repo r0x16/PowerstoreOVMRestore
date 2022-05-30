f5477e2099583eb649b20eed673e762f70de1abc
r0x16 <sir.thomas.lara@gmail.com>
2022-05-29 22:47:41 -0400

    Configuration package first version

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/r0x16/PowerstoreOVMRestore/config"
 	"github.com/r0x16/PowerstoreOVMRestore/gui"
 )
 
@@ -10,8 +12,17 @@ var MainWindow *gui.MainWindow
 
 func main() {
 	fmt.Println("START")
+	initConfiguration()
 	MainWindow = gui.NewMainWindow("PowerstoreOVMRestore")
 	MainLayout := gui.NewMainLayout()
 	MainWindow.SetContent(MainLayout)
 	MainWindow.Play()
+}
+
+func initConfiguration() {
+	err := config.Initialize()
+	if err != nil {
+		fmt.Println("Error loading configuration file, application cannot start", err)
+		os.Exit(1)
+	}
 }
