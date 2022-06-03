f55363c7e70d8b705add3f909afca4d4f427f346
r0x16 <sir.thomas.lara@gmail.com>
2022-06-03 18:55:57 -0400

    feature/M001-03 - Size from config Set wizard windoiw size from config

--- sites/infraestructure/view/create.layout.go
+++ sites/infraestructure/view/create.layout.go
@@ -1,12 +1,15 @@
 package view
 
-import "github.com/r0x16/PowerstoreOVMRestore/shared/infraestructure/gui"
+import (
+	"github.com/r0x16/PowerstoreOVMRestore/shared/infraestructure/config"
+	"github.com/r0x16/PowerstoreOVMRestore/shared/infraestructure/gui"
+)
 
 var CreateSiteWizardWindow *gui.Window
 
 func NewCreateSiteWizard() *gui.Window {
 	w := gui.NewWindow("Create Site")
-	w.SetSize(800, 600)
+	w.SetSize(config.Global.WIZZARD_WIDTH, config.Global.WIZZARD_HEIGHT)
 	CreateSiteWizardWindow = w
 	return w
 }
