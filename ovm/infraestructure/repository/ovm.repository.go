b79f4eae52ae13a1ab2bcaa79763778ab93f6ff7
r0x16 <sir.thomas.lara@gmail.com>
2022-06-28 15:25:49 -0400

    Add repository GORM implementation
    feature/M002-06

---
+++ ovm/infraestructure/repository/ovm.repository.go
@@ -0,0 +1,39 @@
+package repository
+
+import (
+	"github.com/r0x16/PowerstoreOVMRestore/ovm/domain/model"
+	"github.com/r0x16/PowerstoreOVMRestore/shared/infraestructure/database"
+
+	i "github.com/r0x16/PowerstoreOVMRestore/ovm/domain/repository" // i is for interface
+	ms "github.com/r0x16/PowerstoreOVMRestore/sites/domain/model"   // ms is for model site
+
+	"gorm.io/gorm"
+)
+
+type OvmRepositoryGorm struct {
+	db *gorm.DB
+}
+
+var _ i.OvmRepository = &OvmRepositoryGorm{}
+
+// Creates a new GORM Ovm Repository
+func NewOvmRepository() *OvmRepositoryGorm {
+	repo := &OvmRepositoryGorm{
+		db: database.Connection,
+	}
+	repo.db.AutoMigrate(&model.Ovm{})
+	return repo
+}
+
+// Get Ovm from database based on site
+func (o *OvmRepositoryGorm) GetBySite(site ms.Site) (model.Ovm, int64, error) {
+	ovm := model.Ovm{}
+	result := o.db.Where("site_id = ?", site.ID).Limit(1).Find(&ovm)
+	return ovm, result.RowsAffected, result.Error
+}
+
+// Store a new ovm in database
+func (o *OvmRepositoryGorm) Store(ovm model.Ovm) (int64, error) {
+	result := o.db.Create(&ovm)
+	return result.RowsAffected, result.Error
+}
