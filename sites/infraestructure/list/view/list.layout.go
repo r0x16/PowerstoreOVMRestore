18bb9fe5f21f613c7b2b9c0f3da9e0ee691c200c
r0x16 <sir.thomas.lara@gmail.com>
2022-07-08 21:45:49 -0400

    Change site container layout
    feature/M001-06
    Changed from widget Accordeon to Simple VBox

--- sites/infraestructure/list/view/list.layout.go
+++ sites/infraestructure/list/view/list.layout.go
@@ -9,17 +9,17 @@ import (
 )
 
 type ListSiteView struct {
-	container   *widget.Accordion
+	container   *fyne.Container
 	SitesLayout []*fyne.Container
 }
 
 // Initializes the container view for listing the sites
 func NewSiteListLayout() *ListSiteView {
-	accordeon := widget.NewAccordion()
+	accordeon := container.NewVBox()
 	listView := &ListSiteView{
 		container: accordeon,
 	}
-	view.SitesContainer.RefreshSiteListLayout(container.NewVBox(listView.container))
+	view.SitesContainer.RefreshSiteListLayout(listView.container)
 	return listView
 }
 
@@ -28,14 +28,17 @@ func (v *ListSiteView) SetSites(sites []model.Site) {
 	v.SitesLayout = make([]*fyne.Container, len(sites))
 	for i, site := range sites {
 		layout := v.siteLayout(&site)
-		v.SitesLayout[i] = layout.Detail.(*fyne.Container)
-		v.container.Append(layout)
+		v.SitesLayout[i] = layout
+		v.container.Add(layout)
 	}
 }
 
 // Create a single site layout item
-func (v *ListSiteView) siteLayout(site *model.Site) *widget.AccordionItem {
-	return widget.NewAccordionItem(site.Name, container.NewVBox())
+func (v *ListSiteView) siteLayout(site *model.Site) *fyne.Container {
+	layout := container.NewVBox(
+		widget.NewLabel(site.Name),
+	)
+	return layout
 }
 
 // Add a new widget to the site
